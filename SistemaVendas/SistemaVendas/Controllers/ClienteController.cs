4ede560f98fd5f3030c02651461710c16f2f71d8
Bia Zoroastro <fabiana.souza@serpro.gov.br>
2020-06-22 12:14:24 -0300

    Lista de clientes

---
+++ SistemaVendas/SistemaVendas/Controllers/ClienteController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaVendas.Models;
+
+namespace SistemaVendas.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.ListaClientes = new ClienteModel().ListarTodosClientes();
+            return View();
+        }
+    }
+}
---
+++ SistemaVendas/SistemaVendas/Models/ClienteModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using SistemaVendas.Uteis;
+
+namespace SistemaVendas.Models
+{
+    public class ClienteModel
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+
+        public List<ClienteModel> ListarTodosClientes()
+        {
+            List<ClienteModel> lista = new List<ClienteModel>();
+            ClienteModel item;
+            string sql = $"select id, nome, cpf_cnpj, email, senha FROM cliente ORDER BY nome asc";
+            DAL objDAL = new DAL();
+            DataTable dt = objDAL.RetDataTable(sql);
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                item = new ClienteModel
+                {
+                    Id = dt.Rows[0]["id"].ToString(),
+                    Nome = dt.Rows[0]["nome"].ToString(),
+                    CPF = dt.Rows[0]["cpf_cnpj"].ToString(),
+                    Email = dt.Rows[0]["email"].ToString(),
+                    Senha = dt.Rows[0]["senha"].ToString(),
+                };
+                lista.Add(item);
+            }
+
+            return lista;
+
+        }
+    }
+}
