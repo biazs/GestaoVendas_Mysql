fc3389649aaa5ea995a2f10255348eabb39cba24
Bia Zoroastro <fabiana.souza@serpro.gov.br>
2020-06-23 15:29:42 -0300

    CRUD - Produtos - Parte 2

--- SistemaVendas/SistemaVendas/Models/ProdutoModel.cs
+++ SistemaVendas/SistemaVendas/Models/ProdutoModel.cs
@@ -15,12 +15,24 @@ namespace SistemaVendas.Models
         [Required(ErrorMessage = "Informe a descrição do produto.")]
         public string Descricao { get; set; }
 
+        [Required(ErrorMessage = "Informe o preço unitário do produto.")]
+        public decimal Preco_Unitario { get; set; }
+
+        [Required(ErrorMessage = "Informe a quantidade em estoque do produto.")]
+        public decimal Quantidade_Estoque { get; set; }
+
+        [Required(ErrorMessage = "Informe a unidade de medida do produto.")]
+        public string Unidade_Medida { get; set; }
+
+        [Required(ErrorMessage = "Informe o link da imagem do produto.")]
+        public string Link_Foto { get; set; }
+
 
         public List<ProdutoModel> ListarTodosProdutos()
         {
             List<ProdutoModel> lista = new List<ProdutoModel>();
             ProdutoModel item;
-            string sql = $"select id, nome, descricao FROM produto ORDER BY nome asc";
+            string sql = $"select id, nome, descricao, preco_unitario, quantidade_estoque, unidade_medida, link_foto FROM produto ORDER BY nome asc";
             DAL objDAL = new DAL();
             DataTable dt = objDAL.RetDataTable(sql);
 
@@ -31,6 +43,10 @@ namespace SistemaVendas.Models
                     Id = dt.Rows[i]["id"].ToString(),
                     Nome = dt.Rows[i]["nome"].ToString(),
                     Descricao = dt.Rows[i]["descricao"].ToString(),
+                    Preco_Unitario = decimal.Parse(dt.Rows[i]["preco_unitario"].ToString()),
+                    Quantidade_Estoque = decimal.Parse(dt.Rows[i]["quantidade_estoque"].ToString()),
+                    Unidade_Medida = dt.Rows[i]["unidade_medida"].ToString(),
+                    Link_Foto = dt.Rows[i]["link_foto"].ToString(),
                 };
                 lista.Add(item);
             }
@@ -41,7 +57,7 @@ namespace SistemaVendas.Models
         public ProdutoModel RetornarProduto(int? id)
         {
             ProdutoModel item;
-            string sql = $"select id, nome, descricao FROM produto WHERE id = '{id}' ORDER BY nome asc";
+            string sql = $"select id, nome, descricao, preco_unitario, quantidade_estoque, unidade_medida, link_foto FROM produto WHERE id = '{id}' ORDER BY nome asc";
             DAL objDAL = new DAL();
             DataTable dt = objDAL.RetDataTable(sql);
 
@@ -50,6 +66,10 @@ namespace SistemaVendas.Models
                 Id = dt.Rows[0]["id"].ToString(),
                 Nome = dt.Rows[0]["nome"].ToString(),
                 Descricao = dt.Rows[0]["descricao"].ToString(),
+                Preco_Unitario = decimal.Parse(dt.Rows[0]["preco_unitario"].ToString()),
+                Quantidade_Estoque = decimal.Parse(dt.Rows[0]["quantidade_estoque"].ToString()),
+                Unidade_Medida = dt.Rows[0]["unidade_medida"].ToString(),
+                Link_Foto = dt.Rows[0]["link_foto"].ToString(),
             };
 
             return item;
@@ -62,11 +82,19 @@ namespace SistemaVendas.Models
 
             if (Id != null)
             {
-                sql = $"UPDATE produto SET nome = '{Nome}', descricao = '{Descricao}' WHERE id = '{Id}'";
+                sql = $"UPDATE produto SET " +
+                    $"nome = '{Nome}', " +
+                    $"descricao = '{Descricao}', " +
+                    $"preco_unitario = '{Preco_Unitario.ToString().Replace(",", ".")}', " +
+                    $"quantidade_estoque = '{Quantidade_Estoque}', " +
+                    $"unidade_medida = '{Unidade_Medida}', " +
+                    $"link_foto = '{Link_Foto}' " +
+                    $"WHERE id = '{Id}'";
             }
             else
             {
-                sql = $"INSERT into produto(nome, descricao) VALUES ('{Nome}','{Descricao}')";
+                sql = $"INSERT into produto(nome, descricao, preco_unitario, quantidade_estoque, unidade_medida, link_foto ) " +
+                      $"VALUES ('{Nome}','{Descricao}', '{Preco_Unitario}','{Quantidade_Estoque}', '{Unidade_Medida}','{Link_Foto}')";
             }
 
             objDAL.ExecutarComandoSQL(sql);
