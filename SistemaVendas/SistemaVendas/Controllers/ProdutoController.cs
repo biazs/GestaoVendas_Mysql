12f8b4f15fa67cacae71288feb5931ca9f73eff8
Bia Zoroastro <fabiana.souza@serpro.gov.br>
2020-06-22 22:55:33 -0300

    CRUD Produto

---
+++ SistemaVendas/SistemaVendas/Controllers/ProdutoController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaVendas.Models;
+
+namespace SistemaVendas.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.ListaProdutos = new ProdutoModel().ListarTodosProdutos();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Cadastro(int? id)
+        {
+            if (id != null)
+            {
+                ViewBag.Produto = new ProdutoModel().RetornarProduto(id);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(ProdutoModel produto)
+        {
+            if (ModelState.IsValid)
+            {
+                produto.Gravar();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Excluir(int id)
+        {
+            ViewData["idExcluir"] = id;
+            return View();
+        }
+
+        public IActionResult ExcluirProduto(int id)
+        {
+            new ProdutoModel().Excluir(id);
+            return View();
+        }
+    }
+}
---
+++ SistemaVendas/SistemaVendas/Models/ProdutoModel.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using SistemaVendas.Uteis;
+
+namespace SistemaVendas.Models
+{
+    public class ProdutoModel
+    {
+        public string Id { get; set; }
+        [Required(ErrorMessage = "Informe o nome do produto.")]
+        public string Nome { get; set; }
+
+
+        [Required(ErrorMessage = "Informe a descrição do produto.")]
+        public string Descricao { get; set; }
+
+
+        public List<ProdutoModel> ListarTodosProdutos()
+        {
+            List<ProdutoModel> lista = new List<ProdutoModel>();
+            ProdutoModel item;
+            string sql = $"select id, nome, descricao FROM produto ORDER BY nome asc";
+            DAL objDAL = new DAL();
+            DataTable dt = objDAL.RetDataTable(sql);
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                item = new ProdutoModel
+                {
+                    Id = dt.Rows[i]["id"].ToString(),
+                    Nome = dt.Rows[i]["nome"].ToString(),
+                    Descricao = dt.Rows[i]["descricao"].ToString(),
+                };
+                lista.Add(item);
+            }
+
+            return lista;
+        }
+
+        public ProdutoModel RetornarProduto(int? id)
+        {
+            ProdutoModel item;
+            string sql = $"select id, nome, descricao FROM produto WHERE id = '{id}' ORDER BY nome asc";
+            DAL objDAL = new DAL();
+            DataTable dt = objDAL.RetDataTable(sql);
+
+            item = new ProdutoModel
+            {
+                Id = dt.Rows[0]["id"].ToString(),
+                Nome = dt.Rows[0]["nome"].ToString(),
+                Descricao = dt.Rows[0]["descricao"].ToString(),
+            };
+
+            return item;
+        }
+
+        public void Gravar()
+        {
+            DAL objDAL = new DAL();
+            string sql = string.Empty;
+
+            if (Id != null)
+            {
+                sql = $"UPDATE produto SET nome = '{Nome}', descricao = '{Descricao}' WHERE id = '{Id}'";
+            }
+            else
+            {
+                sql = $"INSERT into produto(nome, descricao) VALUES ('{Nome}','{Descricao}')";
+            }
+
+            objDAL.ExecutarComandoSQL(sql);
+
+        }
+
+        public void Excluir(int id)
+        {
+            DAL objDAL = new DAL();
+            string sql = $"DELETE FROM produto WHERE id = '{id}'";
+            objDAL.ExecutarComandoSQL(sql);
+        }
+    }
+}
