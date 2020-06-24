de42b430c8ff616232f6007edece19d6b246b978
Bia Zoroastro <fabiana.souza@serpro.gov.br>
2020-06-24 16:07:32 -0300

    Relatório de pizza

--- SistemaVendas/SistemaVendas/Controllers/RelatorioController.cs
+++ SistemaVendas/SistemaVendas/Controllers/RelatorioController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 using SistemaVendas.Models;
 
 namespace SistemaVendas.Controllers
@@ -34,6 +36,27 @@ namespace SistemaVendas.Controllers
 
         public IActionResult Grafico()
         {
+            List<GraficoProdutos> lista = new GraficoProdutos().RetornarGrafico();
+            string valores = "";
+            string labels = "";
+            string cores = "";
+
+            var random = new Random();
+
+            //Percorre a lista de itens para compor o gráfico
+            for (int i = 0; i < lista.Count; i++)
+            {
+                valores += lista[i].QtdeVendido.ToString() + ",";
+                labels += "'" + lista[i].DescricaoProduto.ToString() + "',";
+
+                //Escolher aleatoriamente as cores para compor o gráfico tipo torta
+                cores += "'" + String.Format("#{0:X6}", random.Next(0x1000000)) + "',";
+            }
+
+            ViewBag.Valores = valores;
+            ViewBag.Labels = labels;
+            ViewBag.Cores = cores;
+
             return View();
         }
         public IActionResult Comissao()
--- SistemaVendas/SistemaVendas/Models/RelatorioModel.cs
+++ SistemaVendas/SistemaVendas/Models/RelatorioModel.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using SistemaVendas.Uteis;
 
 namespace SistemaVendas.Models
 {
@@ -7,4 +10,37 @@ namespace SistemaVendas.Models
         public DateTime DataDe { get; set; }
         public DateTime DataAte { get; set; }
     }
+
+    public class GraficoProdutos
+    {
+        public double QtdeVendido { get; set; }
+        public int CodigoProduto { get; set; }
+        public string DescricaoProduto { get; set; }
+
+        public List<GraficoProdutos> RetornarGrafico()
+        {
+            DAL objDAL = new DAL();
+            string sql = "SELECT SUM(qtde_produto) as qtde, p.nome as produto " +
+                         "FROM sistema_venda.itens_venda i inner join sistema_venda.produto p " +
+                         "on i.produto_id = p.id GROUP BY p.nome";
+
+            DataTable dt = objDAL.RetDataTable(sql);
+
+            List<GraficoProdutos> lista = new List<GraficoProdutos>();
+            GraficoProdutos item;
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                item = new GraficoProdutos();
+                item.QtdeVendido = double.Parse(dt.Rows[i]["qtde"].ToString());
+                item.DescricaoProduto = dt.Rows[i]["produto"].ToString();
+
+                lista.Add(item);
+            }
+
+            return lista;
+        }
+    }
 }
+
+
